0371f4e384db80f1fe2f20869696fba0ce8fc0cb
Lasse Hjort <lhjort18@student.aau.dk>
2020-04-23 13:45:48 +0200

    guest full

--- P4FormsTest2/Guest.cs
+++ P4FormsTest2/Guest.cs
@@ -3,5 +3,22 @@
     public class Guest
     {
         public static int TotalId { get; set; } = 0;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public Reservation[] PreviousReservations { get; set; } //Ved ikke lige med den her
+        public bool IsMember { get; set; }
+
+        public Guest(string name, string phoneNumber, string email, Reservation[] previousReservations, bool isMember)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            PreviousReservations = previousReservations;
+            IsMember = isMember;
+            TotalId = TotalId + 1;
+            Id = TotalId;
+        }
     }
 }

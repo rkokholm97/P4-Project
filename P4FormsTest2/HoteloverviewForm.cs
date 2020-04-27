1641dd61c52b9af4dd70708a28676447b46858ac
Rasmus Brandt Sørensen <rbsa17@student.aau.dk>
2020-04-27 11:51:00 +0200

    hotel overview form first draft

--- P4FormsTest2/HoteloverviewForm.cs
+++ P4FormsTest2/HoteloverviewForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace P4FormsTest2
@@ -36,6 +29,26 @@ namespace P4FormsTest2
             GuestmanagementForm guestmanagementform = new GuestmanagementForm();
             guestmanagementform.Closed += (s, args) => this.Close();
             guestmanagementform.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
